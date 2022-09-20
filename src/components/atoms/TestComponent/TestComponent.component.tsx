93bb22887a32f696b340a7889f0c5e0069644bd2
abacchi00 <andre.rossini.bacchi@gmail.com>
2022-09-20 02:23:52 -0300

    chore: enhance TestComponent

---
+++ src/components/atoms/TestComponent/TestComponent.component.tsx
@@ -0,0 +1,11 @@
+import { TestContainer } from './TestComponent.styles';
+
+const TestComponent = () => {
+  return (
+    <TestContainer>
+      <p>Test component for Dropull's challenge</p>
+    </TestContainer>
+  );
+}
+
+export default TestComponent;
--- src/components/atoms/TestComponent/TestComponent.styles.ts
+++ src/components/atoms/TestComponent/TestComponent.styles.ts
@@ -1,6 +1,8 @@
 import styled from '@emotion/styled';
 
 export const TestContainer = styled.div`
-  background-color: green;
+  background-color: ${props => props.theme.colors.dark[400]};
   border-radius: ${props => props.theme.borderRadius.medium};
+  padding: 16px;
+  margin: 16px;
 `;
