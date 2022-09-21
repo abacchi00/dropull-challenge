bae230db17acedfc61b7d2a645adbc28d7ea7d9b
abacchi00 <andre.rossini.bacchi@gmail.com>
2022-09-20 23:13:14 -0300

    chore: delete TestComponent

--- src/components/atoms/TestComponent/TestComponent.styles.ts
+++
@@ -1,8 +0,0 @@
-import styled from '@emotion/styled';
-
-export const TestContainer = styled.div`
-  background-color: ${({ theme }) => theme.colors.dark[400]};
-  border-radius: ${({ theme }) => theme.borderRadius.medium};
-  padding: ${({ theme }) => theme.spacing.medium};
-  margin: ${({ theme }) => theme.spacing.medium};
-`;
